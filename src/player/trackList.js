2b3482db4b606d66d5d6db0a9e918158f72be874
Dobre Robert <dobrerobert441@gmail.com>
2024-10-20 11:43:04 +0300

    info updated

--- src/player/trackList.js
+++ src/player/trackList.js
@@ -3,28 +3,34 @@ function getTrackList() {
     let songCount = 0;
 
     const trackList = [
-        { name: 'Cmon', id: songCount++, 
-        duration: '4:01', plays: 316977 },
-        { name: 'Morning mudd', id: songCount++,
-        duration: '3:69', plays: 690000 },
-        { name: 'Got rich', id: songCount++, 
-        duration: '2:28', plays: 16977 },
-        { name: 'Let ya know', id: songCount++, 
-        duration: '5:16', plays: 16977 },
-        { name: 'Stayed the same', id: songCount++, 
-        duration: '3:09', plays: 1116977 },
-        { name: 'Get busy', id: songCount++, 
-        duration: '4:59', plays: 16977 },
+        { name: 'Nikes', id: songCount++, 
+        duration: '5:14', plays: 339450 },
+        { name: 'Ivy', id: songCount++,
+        duration: '4:09', plays: 903042 },
+        { name: 'Pink + White', id: songCount++, 
+        duration: '2:28', plays: 506645 },
+        { name: 'Be Yourself', id: songCount++, 
+        duration: '1:26', plays: 83263 },
+        { name: 'Solo', id: songCount++, 
+        duration: '4:17', plays: 251149 },
+        { name: 'Skyline To', id: songCount++, 
+        duration: '3:04', plays: 118760 },
+        { name: 'Self Control', id: songCount++, 
+        duration: '4:09', plays: 483100 },
+        { name: 'Good Guy', id: songCount++, 
+        duration: '1:06', plays: 106995 },
+        { name: 'Nights', id: songCount++, 
+        duration: '5:07', plays: 766593 },
     ];
 
     const AlbumInfo = [
         {
-            name: 'I AM STILL WATER',
-            artist: 'Playboi carti',
+            name: 'AN ALBUM NAME',
+            artist: 'Artist',
             album: true,
             date: [7, ' Nov'],
             year: 2024,
-            label: '00pium',
+            label: 'Label',
             songs: songCount
         }
     ]
