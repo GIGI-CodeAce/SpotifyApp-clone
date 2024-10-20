7ba4f4576a8e67a5d54945d2f3af407c4c53efa0
Dobre Robert <dobrerobert441@gmail.com>
2024-10-20 11:30:21 +0300

    flexibility increased

--- src/player/album.js
+++ src/player/album.js
@@ -18,12 +18,19 @@ function Album(props) {
     document.getElementById('imageInput').click();
   };
 
+  const [like,setLike]=useState(false)
+
+  function Add(){
+    setLike((oldVal)=> !oldVal)
+    return ""
+  }
+
   return (
-    <div className="bg-stone-950 h-80">
+    <div className="bg-gradient-to-b from-slate-700 via-black to-black h-80 overflow-hidden">
       <div className="inline-flex">
         {/* Photo */}
         <div
-          className="hover:cursor-pointer h-56 w-60 border-2 ml-4 rounded-lg shadow-md shadow-white"
+          className="hover:cursor-pointer h-52 sm:h-56 sm:w-56 w-48 border-2 ml-4 rounded-lg shadow-md shadow-white"
           style={{
             backgroundImage: `url(${image})`,
             backgroundSize: 'cover',
@@ -35,26 +42,27 @@ function Album(props) {
         </div>
 
         {/* Song title */}
-        <div className="absolute ml-72 mt-10 w-6 h-6 text-white">{props.album ? 'Album' : 'Single'}</div>
-        <div className="absolute ml-72 mt-20 text-3xl sm:text-5xl text-white">{props.name}</div>
-        <div className="absolute ml-72 mt-36 text-md text-white">{props.artist} • {props.year} • {props.songs} songs</div>
+        <div className="absolute font-semibold ml-60 sm:ml-72 mt-10 w-6 h-6 text-white">{props.album ? 'Album' : 'Single'}</div>
+        <div className="absolute font-semibold ml-60 mt-[89px] sm:mt-20 xl:text-5xl lg:text-4xl md:text-3xl sm:text-3xl sm:ml-72 text-white
+         max-w-[400px] overflow-hidden max-h-13 whitespace-nowrap" contentEditable spellCheck="false"  >{props.name}</div>
+        <div className="absolute ml-60 sm:ml-72 mt-36 text-xs text-white md:text-2xl sm:text-xl ">{props.artist} • {props.year} • {props.songs} songs</div>
       </div>
 
       <br />
       <main className="relative">
         <div className="inline-flex h-16 border-4 rounded-[50%] border-green-500 bg-green-500 w-16 m-4">
-          <span className="text-black hover:cursor-pointer select-none">
+          <abbr title='Play'><span className="text-black hover:cursor-pointer select-none">
             <span className="material-symbols-outlined text-[58px]">play_arrow</span>
-          </span>
+          </span></abbr>
         </div>
-        <span className="text-white absolute mt-6 left-12 hover:cursor-pointer select-none">
+        <abbr title='Shuffle'><span className="text-white absolute mt-6 left-12 hover:cursor-pointer select-none">
           <span className="material-symbols-outlined text-[50px] absolute left-11">shuffle</span>
-        </span>
-        <span className="text-white absolute mt-6 left-40 hover:cursor-pointer select-none">
+        </span></abbr>
+        <abbr title='Save'><span onClick={Add} className="text-white absolute mt-6 left-40 hover:cursor-pointer select-none">
           <span className="material-symbols-outlined text-[50px]">add_circle</span>
-        </span>
+        </span></abbr>
         <span className="text-white absolute right-2 mt-11 mr-2 hover:cursor-pointer select-none">
-          <span className="material-symbols-outlined text-3xl">list</span>
+        <span className="material-symbols-outlined text-3xl">list</span>
         </span>
       </main>
 
