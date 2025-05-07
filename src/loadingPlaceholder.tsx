3de64f2a6c1bf66dac5ddcd998741fa183020d59
gigi <dobrerobert441@gmail.com>
2025-05-07 19:45:49 +0300

    Hotfixes

--- src/loadingPlaceholder.tsx
+++ src/loadingPlaceholder.tsx
@@ -5,21 +5,22 @@ function LoadingPlaceholder(){
               <div className="relative bg-gradient-to-b pt-2 mb-4 from-slate-700 via-black to-black h-80 overflow-hidden">
               <div className="inline-flex">
         <div
-            className="h-52 cursor-pointer sm:h-56 sm:w-56 w-48 bg-slate-800 animate-pulse ml-4 rounded-lg border-1 shadow-md "
-          ></div>
+            className="h-52 cursor-pointer sm:h-56 sm:w-56 w-48 bg-slate-800 animate-pulse ml-4 rounded-lg border-1 shadow-md ">
+              {/* Album cover */}
+          </div>
   
-          <div className="absolute font-semibold bg-slate-800 animate-pulse ml-60 h-10 sm:w-30 w-20 rounded-xl sm:ml-72 mt-10 "></div>
-          <div className="absolute bg-slate-800 text-green-600 text-center justify-center pt-2 animate-pulse ml-60 h-10 sm:w-60 w-40 rounded-xl mt-[89px] sm:ml-72">
+          <div className="absolute font-semibold bg-slate-800 animate-pulse ml-56 h-10 sm:w-30 w-20 rounded-xl sm:ml-72 mt-10 ">
+            {/* Album text */}
+          </div>
+          <div className="absolute bg-slate-800 text-green-600 text-center justify-center pt-2 animate-pulse ml-55 h-10 sm:w-60 w-40 rounded-xl mt-[89px] sm:ml-72">
             {/* Album name */}Loading API..
           </div>
-          <div className="absolute bg-slate-800 h-6 w-18 rounded-xl animate-pulse sm:ml-72 ml-60 mt-36 ">
+          <div className="absolute bg-slate-800 h-6 w-18 rounded-xl animate-pulse sm:ml-72 ml-55 mt-36 ">
              {/* Artist name */}
           </div>
           <div className="absolute bg-slate-800 w-13 h-6 animate-pulse rounded-xl sm:ml-92 ml-80 mt-36 ">
-             
           </div>
           <div className="absolute bg-slate-800 w-10 h-6 animate-pulse hidden sm:block rounded-xl sm:ml-[429px] ml-95 mt-36 ">
-             
              </div>
         </div>
   
@@ -55,6 +56,12 @@ function LoadingPlaceholder(){
             <span className="absolute animate-pulse w-10 h-5 rounded-2xl bg-slate-700 right-[10%] top-1/2 transform -translate-y-1/2"></span>
     </div>
       <ol className="list-disc list-outside marker:text-slate-700 pr-2 pl-5 ml-1 border-4 border-black bg-black">
+          <li
+            className="rounded-md bg-[hsl(215,28%,14%)] mb-2 py-2 pr-2 pl-4 h-11 relative">
+            <span className="ml-1 animate-pulse w-30 sm:w-40 h-6 bg-slate-700 rounded-xl inline-block max-w-[200px] sm:max-w-[500px] truncate"></span>
+            <span className="absolute right-[35%] w-20 h-6 bg-slate-700 rounded-xl animate-pulse"></span>
+            <span className="absolute right-[10%] sm:w-20 w-15 h-6 bg-slate-700 rounded-xl animate-pulse"></span>
+          </li>
           <li
             className="rounded-md bg-[hsl(215,28%,14%)] mb-2 py-2 pr-2 pl-4 h-11 relative">
             <span className="ml-1 animate-pulse w-30 sm:w-40 h-6 bg-slate-700 rounded-xl inline-block max-w-[200px] sm:max-w-[500px] truncate"></span>
--- src/player/footerTrack.tsx
+++ src/player/footerTrack.tsx
@@ -6,12 +6,12 @@ const Footer = ({ album }: { album: AlbumProps }) => {
     return(
         <div className=" text-white opacity-[40%] ml-4 mt-5 h-40 leading-5">
             <span className="hover:text-green-400">{album.date}</span><br/>
-            <span className="hover:text-green-400 text-xs">©{album.year_date} {album.name}</span><br/><hr/><br/>
+            <span className="hover:text-green-400 text-xs">©{album.year_date} {album.name}</span><br/><br/><hr/><br/>
             <div className="leading-6 inline-grid">
-            <span className='hover:text-green-400 w-[10000px]'>v1.5.0</span>
+            <span className='hover:text-green-400 w-[10000px]'>v1.5.1</span>
             <span className='hover:text-green-400'>Roby/GIGI</span>
-            <a href="https://github.com/GIGI-CodeAce" className='hover:text-green-400'>Github</a>
-            </div>
+            <a href="https://github.com/GIGI-CodeAce" className='hover:text-green-400 hover:underline'>Github</a>
+            </div><br/><br/>
         </div>
     )
 }
